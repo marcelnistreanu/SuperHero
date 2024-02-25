0edec8f18221a72a20c553f499a2e04650599a57
marcelnistreanu <marcelnistreanu@gmail.com>
2024-02-25 23:06:30 +0200

    Add Utils folder

---
+++ SuperHeroAPI/Utils/Guard.cs
@@ -0,0 +1,12 @@
+﻿namespace SuperHeroAPI.Utils;
+
+public static class Guard
+{
+    public static void Require(bool condition, string errorMessage)
+    {
+        if (!condition)
+        {
+            throw new ArgumentException(errorMessage);
+        }
+    }
+}
---
+++ SuperHeroAPI/Utils/ModelStateValidator.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SuperHeroAPI.Utils;
+
+public class ModelStateValidator
+{
+    public static IActionResult ValidateModelState(ActionContext context)
+    {
+        (string fieldName, ModelStateEntry entry) = context.ModelState
+            .First(x => x.Value.Errors.Count > 0);
+        string errorSerialized = entry.Errors[1].ErrorMessage;
+
+        Error error = Error.Deserialize(errorSerialized);
+        Envelope envelope = Envelope.Error(error, fieldName);
+        var result = new BadRequestObjectResult(envelope);
+
+        return result;
+    }
+}

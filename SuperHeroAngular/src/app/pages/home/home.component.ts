607e5ebc91d3fbe3c7b48bd70c52e2afbd5f186c
marcelnistreanu <marcelnistreanu@gmail.com>
2024-03-18 22:34:24 +0200

    Modify home

--- SuperHeroAngular/src/app/pages/home/home.component.ts
+++ SuperHeroAngular/src/app/pages/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SuperHero } from '../../models/SuperHero';
-import { SuperHeroService } from '../../super-hero.service';
+import { SuperHeroService } from '../../services/super-hero.service';
 
 @Component({
   selector: 'app-home',
@@ -9,8 +9,9 @@ import { SuperHeroService } from '../../super-hero.service';
 })
 export class HomeComponent implements OnInit {
 
-  superheroes: SuperHero[];
-  hero: SuperHero = new SuperHero();
+  superheroes: SuperHero[] = [];
+  hero: SuperHero;
+    //= new SuperHero(10, "Super", "Vas", "AS", "asd");
 
   constructor(private superHeroesService: SuperHeroService) { }
 
@@ -23,13 +24,42 @@ export class HomeComponent implements OnInit {
     this.superHeroesService.getSuperHeroes().subscribe((response: any) => {
       if (response && response.value) {
         this.superheroes = response.value;
-        this.hero = this.superheroes[0];
+        //this.hero = this.superheroes[0];
         console.log("Lista:", this.superheroes);
         console.log(this.hero);
       } else {
         console.error("Invalid response format:", response);
       }
     });
+  }
+
+  getHeroesNew(): void {
+    console.log("Sending HTTP request to get superheroes");
+    this.superHeroesService.getHeroesNew().subscribe(
+      (data: SuperHero[]) => {
+        this.superheroes = data;
+        console.log(this.superheroes);
+      },
+      (error) => {
+        console.error('Error fetching superheroes:', error);
+      }
+    );
+  }
+
+
+  deleteHero(heroId: number): void {
+    console.log("Sending HTTP request to delete superhero");
+    this.superHeroesService.deleteHero(heroId).subscribe((response: any) => {
+      if (response) {
+        console.log("Hero deleted");
+      } else {
+        console.error("Invalid response format:", response);
+      }
+    })
+  }
+
+  selectHero(heroName: string): void {
+    console.log(heroName);
   }
 
 }
